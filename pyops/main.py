f04fd6e8039622fc742951bce1302a666cefccc7
ChandanCVM <119293412+ChandanCVM@users.noreply.github.com>
2023-06-12 17:31:52 +0530

    added file

---
+++ pyops/main.py
@@ -0,0 +1,8 @@
+# Prompt the user to enter two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+# Add the two numbers
+result = num1 + num2
+
+# Display the result
+print("The sum of", num1, "and", num2, "is", round(result))
