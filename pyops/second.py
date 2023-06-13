8b75aa059077689a4d34a868ff4d91d32d2f999d
RVSHZ66 <136419784+RVSHZ66@users.noreply.github.com>
2023-06-13 13:20:21 +0530

    Delete pyops directory

--- pyops/second.py
+++
@@ -1,8 +0,0 @@
-# Prompt the user to enter two numbers
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-# Add the two numbers
-result = num1 * num2
-
-# Display the result
-print("The Multiplication of", num1, "and", num2, "is", round(result))
